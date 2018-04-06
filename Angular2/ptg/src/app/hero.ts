9f275622b4f830c99c27d6035c61dc2d634d86e5
Lendvai Ferenc <szgt64@gmail.com>
2018-04-06 09:40:08 +0200

    ESO healer

---
+++ Angular2/ptg/src/app/hero.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by lendvai on 2017.05.09.
+ */
+
+export class Hero {
+  id: number;
+  name: string;
+}
---
+++ Angular2/ptg/src/app/search.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'search-tel',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+}
